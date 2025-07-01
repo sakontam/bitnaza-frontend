db29e982293ba35b84cd83a590bb686ea197611b
sakontam <kornnaza588@gmail.com>
2025-07-01 23:17:03 +0700

    fix: Correct import path casing for HomeView component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "../views/homeView.vue";
 import BitcoinView from "../views/bitcoinView.vue";
 import UsdView from "../views/usd_thbView.vue";
 
