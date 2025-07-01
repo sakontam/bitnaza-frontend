45f7960572f9eff19fedb5ef6df89bc717110048
sakontam <kornnaza588@gmail.com>
2025-07-01 23:14:28 +0700

    fix: Correct hostBaseApiUrl configuration for production environment

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,6 @@ export default {
   appName: "My Vue App",
   theme: "dark",
   version: "1.0.0",
-  hostBaseApiUrl: "http://localhost:5000",
-  // hostBaseApiUrl: "https://bitnaza-backend.onrender.com",
+  // hostBaseApiUrl: "http://localhost:5000",
+  hostBaseApiUrl: "https://bitnaza-backend.onrender.com",
 };
